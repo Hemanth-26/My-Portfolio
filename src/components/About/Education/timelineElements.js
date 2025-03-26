627735f3e12c01894fe5e79eada7ea98b79317b5
Hemanth <hemanth26800@gmail.com>
2025-03-26 15:04:49 +0530

    added the new experience

--- src/components/About/Education/timelineElements.js
+++ src/components/About/Education/timelineElements.js
@@ -1,44 +1,84 @@
 let timelineElements = [
     {
         id: 1,
-        title: "Searching a Job!",
-        subTitle: "Frontend Developer",
+        title: "Seeking Opportunities in Full Stack, Frontend, or Backend Development",
+        subTitle: "Full Stack Developer (MERN) | Frontend Developer | Backend Developer",
         location: "Chennai, Tamil Nadu",
         description:
-            "A front end developer has one general responsibility: to ensure that website visitors can easily interact with the page. They do this through the combination of design, technology and programming to code a website's appearance, as well as taking care of debugging.",
-        date: " - present",
+            "Experienced MERN Stack Developer with 3+ years of expertise in building scalable web applications using React.js, Redux, Node.js, Express.js, and MongoDB. Skilled in developing RESTful APIs, implementing authentication (OAuth & JWT), optimizing performance, and ensuring seamless user experiences. Passionate about UI/UX improvements, state management, and working in Agile environments.",
+        date: "Present",
         icon: "job",
     },
     {
         id: 2,
-        title: "Bachelor of Computer Application",
-        subTitle: "Jaya College Of Arts And Science",
-        location: "Thiruninravur, Tamil Nadu",
+        title: "Software Developer",
+        subTitle: "Devship Private Limited",
+        location: "Chennai, Tamil Nadu",
         description:
-            "In my college life, along with study, I and my friends enjoyed a lot of other things. We travelled lots of places, had new experiences and learned many new things. Our college's canteen was the remarkable place of my college life as whenever we got time, we used to chill in the canteen.",
-        date: "2020 - 2017",
-        icon: "work",
+            "Designed and developed scalable full-stack web applications using React.js, Redux, Node.js, Express.js, and MongoDB. Optimized database performance by 30%, implemented secure authentication (OAuth & JWT), and improved system efficiency through code reviews, debugging, and seamless API integrations.",
+        date: "Sep 2023 - Present",
+        icon: "school",
     },
     {
         id: 3,
-        title: "Higher Secondary Certificate",
-        subTitle: "ICF Higher Secondary School",
+        title: "Software Developer",
+        subTitle: "Regami Solution",
         location: "Chennai, Tamil Nadu",
         description:
-            "School life is one of the most important phases in the life of a man. It is the period during which one learns the basic principles of behavior, speech, and etiquette. School life, in fact, is a period of learning and training in all the aspects of the personality of a man.",
-        date: "2017 - 2015",
+            "Engineered responsive and high-performance web applications using React.js, Material UI, and Node.js. Developed custom reusable UI components, integrated third-party APIs for real-time data synchronization, and implemented performance optimizations, reducing page load time by 25%.",
+        date: "Jun 2022 - Jan 2023",
         icon: "school",
     },
     {
         id: 4,
-        title: "Secondary School Leaving Certificate",
-        subTitle: "ICF Higher Secondary School",
+        title: "Senior Front-End Developer",
+        subTitle: "Artifintel Private Limited",
         location: "Chennai, Tamil Nadu",
         description:
-            "School life is one of the most important phases in the life of a man. It is the period during which one learns the basic principles of behavior, speech, and etiquette. School life, in fact, is a period of learning and training in all the aspects of the personality of a man.",
-        date: " 2015",
+            "Led front-end architecture improvements in React.js and Bootstrap, enhancing performance and scalability. Designed and implemented efficient state management solutions using Redux, optimized UI/UX for better responsiveness and rendering speed (15% improvement), and mentored junior developers, strengthening the team’s expertise.",
+        date: "Oct 2021 - Jun 2022",
         icon: "school",
     },
+    {
+        id: 5,
+        title: "Master of Computer Application",
+        subTitle: "University of Madras",
+        location: "Chennai, Tamil Nadu",
+        description:
+            "In my Master’s in Computer Applications (MCA), I continued to deepen my technical knowledge while engaging in activities that broadened my perspective. I gained new experiences, traveled, and further developed my skills. The campus canteen was a central place where I often connected with classmates, fostering a strong sense of community.",
+        date: "2021 - 2023",
+        icon: "work",
+    },
+    {
+        id: 6,
+        title: "Bachelor of Computer Application",
+        subTitle: "Jaya College Of Arts And Science",
+        location: "Thiruninravur, Tamil Nadu",
+        description:
+            "During my Bachelor’s in Computer Applications (BCA), I balanced academics with extracurricular activities that contributed to my personal and professional growth. I explored new places, gained diverse experiences, and developed valuable skills. The campus canteen was a key space where my peers and I often gathered to collaborate and unwind.",
+        date: "2020 - 2017",
+        icon: "work",
+    },
+    // {
+    //     id: 3,
+    //     title: "Higher Secondary Certificate",
+    //     subTitle: "ICF Higher Secondary School",
+    //     location: "Chennai, Tamil Nadu",
+    //     description:
+    //         "School life is one of the most important phases in the life of a man. It is the period during which one learns the basic principles of behavior, speech, and etiquette. School life, in fact, is a period of learning and training in all the aspects of the personality of a man.",
+    //     date: "2017 - 2015",
+    //     icon: "school",
+    // },
+    // {
+    //     id: 4,
+    //     title: "Secondary School Leaving Certificate",
+    //     subTitle: "ICF Higher Secondary School",
+    //     location: "Chennai, Tamil Nadu",
+    //     description:
+    //         "School life is one of the most important phases in the life of a man. It is the period during which one learns the basic principles of behavior, speech, and etiquette. School life, in fact, is a period of learning and training in all the aspects of the personality of a man.",
+    //     date: " 2015",
+    //     icon: "school",
+    // },
 ]
 
 export default timelineElements
